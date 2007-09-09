b29e309e6ed2e8391e38562cd2f172ecf27343cb
amp4ecs <amp4ecs@1bbec0d5-414c-dc11-ae90-00e081727c95>
2007-09-09 18:16:37 +0000

    Upload to warpstock.eu
    git-svn-id: http://svn.netlabs.org/repos/wse_events/trunk@64 1bbec0d5-414c-dc11-ae90-00e081727c95

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "wse_events"
 #
-# Auto generated 09-09-2007 15:45
+# Auto generated 09-09-2007 20:10
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:25:{s:9:"ChangeLog";s:4:"7e85";s:10:"README.txt";s:4:"d74e";s:12:"ext_icon.gif";s:4:"138e";s:17:"ext_localconf.php";s:4:"2215";s:14:"ext_tables.php";s:4:"b308";s:14:"ext_tables.sql";s:4:"a5cc";s:19:"flexform_ds_pi1.xml";s:4:"1335";s:32:"icon_tx_wseevents_categories.gif";s:4:"138e";s:28:"icon_tx_wseevents_events.gif";s:4:"138e";s:31:"icon_tx_wseevents_locations.gif";s:4:"138e";s:27:"icon_tx_wseevents_rooms.gif";s:4:"138e";s:30:"icon_tx_wseevents_sessions.gif";s:4:"138e";s:30:"icon_tx_wseevents_speakers.gif";s:4:"138e";s:31:"icon_tx_wseevents_timeslots.gif";s:4:"138e";s:16:"locallang_db.php";s:4:"80d8";s:7:"tca.php";s:4:"3bf0";s:8:"todo.txt";s:4:"3114";s:14:"wseevents.tmpl";s:4:"d399";s:14:"doc/manual.sxw";s:4:"c57e";s:19:"doc/wizard_form.dat";s:4:"9500";s:20:"doc/wizard_form.html";s:4:"25ab";s:30:"pi1/class.tx_wseevents_pi1.php";s:4:"6e21";s:17:"pi1/locallang.php";s:4:"5987";s:24:"pi1/static/editorcfg.txt";s:4:"87c1";s:20:"pi1/static/setup.txt";s:4:"06fa";}',
+	'_md5_values_when_last_written' => 'a:25:{s:9:"ChangeLog";s:4:"7e85";s:10:"README.txt";s:4:"d74e";s:12:"ext_icon.gif";s:4:"138e";s:17:"ext_localconf.php";s:4:"2215";s:14:"ext_tables.php";s:4:"b308";s:14:"ext_tables.sql";s:4:"a5cc";s:19:"flexform_ds_pi1.xml";s:4:"1335";s:32:"icon_tx_wseevents_categories.gif";s:4:"138e";s:28:"icon_tx_wseevents_events.gif";s:4:"138e";s:31:"icon_tx_wseevents_locations.gif";s:4:"138e";s:27:"icon_tx_wseevents_rooms.gif";s:4:"138e";s:30:"icon_tx_wseevents_sessions.gif";s:4:"138e";s:30:"icon_tx_wseevents_speakers.gif";s:4:"138e";s:31:"icon_tx_wseevents_timeslots.gif";s:4:"138e";s:16:"locallang_db.php";s:4:"80d8";s:7:"tca.php";s:4:"3bf0";s:8:"todo.txt";s:4:"3114";s:14:"wseevents.tmpl";s:4:"bfd9";s:14:"doc/manual.sxw";s:4:"4ab9";s:19:"doc/wizard_form.dat";s:4:"9500";s:20:"doc/wizard_form.html";s:4:"25ab";s:30:"pi1/class.tx_wseevents_pi1.php";s:4:"d18d";s:17:"pi1/locallang.php";s:4:"5987";s:24:"pi1/static/editorcfg.txt";s:4:"87c1";s:20:"pi1/static/setup.txt";s:4:"06fa";}',
 	'suggests' => array(
 	),
 );
