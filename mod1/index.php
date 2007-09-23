bd65c4beb71ed8f736d9735fa03e5612391fc9dc
amp4ecs <amp4ecs@1bbec0d5-414c-dc11-ae90-00e081727c95>
2007-09-23 13:11:11 +0000

    Added tab sheets for database tables.
    git-svn-id: http://svn.netlabs.org/repos/wse_events/trunk@71 1bbec0d5-414c-dc11-ae90-00e081727c95

--- mod1/index.php
+++ mod1/index.php
@@ -2,8 +2,12 @@
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 2007  <>
+*  (c) 2007 Michael Oehlhof (michael@oehlhof.de)
 *  All rights reserved
+*
+*  Because I dont want to redefine the wheel again, some ideas 
+*  and code snippets are taken from the seminar manager extension
+*  tx_seminars
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
 *  free software; you can redistribute it and/or modify
@@ -23,7 +27,7 @@
 ***************************************************************/
 
 
-	// DEFAULT initialization of a module [BEGIN]
+// initialization of the module
 unset($MCONF);
 require_once('conf.php');
 require_once($BACK_PATH.'init.php');
@@ -31,162 +35,333 @@ require_once($BACK_PATH.'template.php');
 
 $LANG->includeLLFile('EXT:wse_events/mod1/locallang.xml');
 require_once(PATH_t3lib.'class.t3lib_scbase.php');
-$BE_USER->modAccess($MCONF,1);	// This checks permissions and exits if the users has no permission for entry.
-	// DEFAULT initialization of a module [END]
+
+
+// This checks permissions and exits if the users has no permission for entry.
+$BE_USER->modAccess($MCONF,1);	
 
 
 
 /**
  * Module 'WSE Events' for the 'wse_events' extension.
  *
- * @author	 <>
+ * @author	 	Michael Oehlhof 
  * @package	TYPO3
  * @subpackage	wse_events
  */
 class  tx_wseevents_module1 extends t3lib_SCbase {
-				var $pageinfo;
+	var $pageinfo;
 
-				/**
-				 * Initializes the Module
-				 * @return	void
-				 */
-				function init()	{
-					global $BE_USER,$LANG,$BACK_PATH,$TCA_DESCR,$TCA,$CLIENT,$TYPO3_CONF_VARS;
+	/** an array of available sub modules */
+	var $availableSubModules;
 
-					parent::init();
-
-					/*
-					if (t3lib_div::_GP('clear_all_cache'))	{
-						$this->include_once[] = PATH_t3lib.'class.t3lib_tcemain.php';
-					}
-					*/
-				}
-
-				/**
-				 * Adds items to the ->MOD_MENU array. Used for the function menu selector.
-				 *
-				 * @return	void
-				 */
-				function menuConfig()	{
-					global $LANG;
-					$this->MOD_MENU = Array (
-						'function' => Array (
-							'1' => $LANG->getLL('function1'),
-							'2' => $LANG->getLL('function2'),
-							'3' => $LANG->getLL('function3'),
-						)
-					);
-					parent::menuConfig();
-				}
-
-				/**
-				 * Main function of the module. Write the content to $this->content
-				 * If you chose "web" as main module, you will need to consider the $this->id parameter which will contain the uid-number of the page clicked in the page tree
-				 *
-				 * @return	[type]		...
-				 */
-				function main()	{
-					global $BE_USER,$LANG,$BACK_PATH,$TCA_DESCR,$TCA,$CLIENT,$TYPO3_CONF_VARS;
-
-					// Access check!
-					// The page will show only if there is a valid page and if this page may be viewed by the user
-					$this->pageinfo = t3lib_BEfunc::readPageAccess($this->id,$this->perms_clause);
-					$access = is_array($this->pageinfo) ? 1 : 0;
-
-					if (($this->id && $access) || ($BE_USER->user['admin'] && !$this->id))	{
-
-							// Draw the header.
-						$this->doc = t3lib_div::makeInstance('mediumDoc');
-						$this->doc->backPath = $BACK_PATH;
-						$this->doc->form='<form action="" method="POST">';
-
-							// JavaScript
-						$this->doc->JScode = '
-							<script language="javascript" type="text/javascript">
-								script_ended = 0;
-								function jumpToUrl(URL)	{
-									document.location = URL;
-								}
-							</script>
-						';
-						$this->doc->postCode='
-							<script language="javascript" type="text/javascript">
-								script_ended = 1;
-								if (top.fsMod) top.fsMod.recentIds["web"] = 0;
-							</script>
-						';
-
-						$headerSection = $this->doc->getHeader('pages',$this->pageinfo,$this->pageinfo['_thePath']).'<br />'.$LANG->sL('LLL:EXT:lang/locallang_core.xml:labels.path').': '.t3lib_div::fixed_lgd_pre($this->pageinfo['_thePath'],50);
-
-						$this->content.=$this->doc->startPage($LANG->getLL('title'));
-						$this->content.=$this->doc->header($LANG->getLL('title'));
-						$this->content.=$this->doc->spacer(5);
-						$this->content.=$this->doc->section('',$this->doc->funcMenu($headerSection,t3lib_BEfunc::getFuncMenu($this->id,'SET[function]',$this->MOD_SETTINGS['function'],$this->MOD_MENU['function'])));
-						$this->content.=$this->doc->divider(5);
+	/** the currently selected sub module */
+	var $subModule;
 
 
-						// Render content:
-						$this->moduleContent();
+	/**
+	 * Initializes the Module
+	 * @return	void
+	 */
+	function init()	{
+		global $BE_USER,$LANG,$BACK_PATH,$TCA_DESCR,$TCA,$CLIENT,$TYPO3_CONF_VARS;
 
-
-						// ShortCut
-						if ($BE_USER->mayMakeShortcut())	{
-							$this->content.=$this->doc->spacer(20).$this->doc->section('',$this->doc->makeShortcutIcon('id',implode(',',array_keys($this->MOD_MENU)),$this->MCONF['name']));
-						}
-
-						$this->content.=$this->doc->spacer(10);
-					} else {
-							// If no access or if ID == zero
-
-						$this->doc = t3lib_div::makeInstance('mediumDoc');
-						$this->doc->backPath = $BACK_PATH;
-
-						$this->content.=$this->doc->startPage($LANG->getLL('title'));
-						$this->content.=$this->doc->header($LANG->getLL('title'));
-						$this->content.=$this->doc->spacer(5);
-						$this->content.=$this->doc->spacer(10);
-					}
-				}
-
-				/**
-				 * Prints out the module HTML
-				 *
-				 * @return	void
-				 */
-				function printContent()	{
-
-					$this->content.=$this->doc->endPage();
-					echo $this->content;
-				}
-
-				/**
-				 * Generates the module content
-				 *
-				 * @return	void
-				 */
-				function moduleContent()	{
-					switch((string)$this->MOD_SETTINGS['function'])	{
-						case 1:
-							$content='<div align="center"><strong>Hello World!</strong></div><br />
-								The "Kickstarter" has made this module automatically, it contains a default framework for a backend module but apart from that it does nothing useful until you open the script '.substr(t3lib_extMgm::extPath('wse_events'),strlen(PATH_site)).$pathSuffix.'index.php and edit it!
-								<hr />
-								<br />This is the GET/POST vars sent to the script:<br />'.
-								'GET:'.t3lib_div::view_array($_GET).'<br />'.
-								'POST:'.t3lib_div::view_array($_POST).'<br />'.
-								'';
-							$this->content.=$this->doc->section('Message #1:',$content,0,1);
-						break;
-						case 2:
-							$content='<div align=center><strong>Menu item #2...</strong></div>';
-							$this->content.=$this->doc->section('Message #2:',$content,0,1);
-						break;
-						case 3:
-							$content='<div align=center><strong>Menu item #3...</strong></div>';
-							$this->content.=$this->doc->section('Message #3:',$content,0,1);
-						break;
-					}
-				}
+		/*
+		 * This is a workaround for the wrong generated links. The workaround is needed to
+		 * get the right values from the GET Parameter. This workaround is from Elmar Hinz
+		 * who also noted this in the bug tracker (http://bugs.typo3.org/view.php?id=2178).
+		 */
+		$matches = array();
+		foreach ($GLOBALS['_GET'] as $key => $value) {
+			if (preg_match('/amp;(.*)/', $key, $matches)) {
+				$GLOBALS['_GET'][$matches[1]] = $value;
 			}
+		}
+		/* --- END OF Workaround --- */
+
+		parent::init();
+
+		$this->id = intval($this->id);
+
+		/*
+		if (t3lib_div::_GP('clear_all_cache'))	{
+			$this->include_once[] = PATH_t3lib.'class.t3lib_tcemain.php';
+		}
+		*/
+	}
+
+	/**
+	 * Adds items to the ->MOD_MENU array. Used for the function menu selector.
+	 *
+	 * @return	void
+	 */
+	function menuConfig()	{
+		global $LANG;
+		$this->MOD_MENU = Array (
+			'function' => Array (
+				'1' => $LANG->getLL('function1'),
+				'2' => $LANG->getLL('function2'),
+				'3' => $LANG->getLL('function3'),
+			)
+		);
+		parent::menuConfig();
+	}
+
+	/**
+	 * Main function of the module. Write the content to $this->content
+	 * If you chose "web" as main module, you will need to consider the $this->id parameter which will contain the uid-number of the page clicked in the page tree
+	 *
+	 * @return	[type]		...
+	 */
+	function main()	{
+		global $BE_USER,$LANG,$BACK_PATH,$TCA_DESCR,$TCA,$CLIENT,$TYPO3_CONF_VARS;
+
+		// Access check!
+		// The page will show only if there is a valid page and if this page may be viewed by the user
+		$this->pageinfo = t3lib_BEfunc::readPageAccess($this->id,$this->perms_clause);
+		$access = is_array($this->pageinfo) ? 1 : 0;
+
+		if (($this->id && $access) || ($BE_USER->user['admin'] && !$this->id))	{
+
+			// Draw the header.
+			$this->doc = t3lib_div::makeInstance('mediumDoc');
+			$this->doc->backPath = $BACK_PATH;
+			$this->doc->form='<form action="" method="POST">';
+
+			// JavaScript
+			$this->doc->JScode = '
+				<script language="javascript" type="text/javascript">
+					script_ended = 0;
+					function jumpToUrl(URL)	{
+						document.location = URL;
+					}
+				</script>
+			';
+			$this->doc->postCode='
+				<script language="javascript" type="text/javascript">
+					script_ended = 1;
+					if (top.fsMod) top.fsMod.recentIds["web"] = 0;
+				</script>
+			';
+
+			$headerSection = $this->doc->getHeader('pages',$this->pageinfo,$this->pageinfo['_thePath']).'<br />'.$LANG->sL('LLL:EXT:lang/locallang_core.xml:labels.path').': '.t3lib_div::fixed_lgd_pre($this->pageinfo['_thePath'],50);
+
+			$this->content.=$this->doc->startPage($LANG->getLL('title'));
+			$this->content.=$this->doc->header($LANG->getLL('title'));
+			$this->content.=$this->doc->spacer(5);
+			$this->content.=$this->doc->section('',$this->doc->funcMenu($headerSection,t3lib_BEfunc::getFuncMenu($this->id,'SET[function]',$this->MOD_SETTINGS['function'],$this->MOD_MENU['function'])));
+			$this->content.=$this->doc->divider(5);
+
+
+			// Render content:
+			$this->moduleContent();
+
+			// For debuging purpose only
+			$this->content .= '<hr />
+						<br />This is the GET/POST vars sent to the script:<br />'.
+						'GET:'.t3lib_div::view_array($_GET).'<br />'.
+						'POST:'.t3lib_div::view_array($_POST).'<br />'.
+						'';
+
+			// ShortCut
+			if ($BE_USER->mayMakeShortcut())	{
+				$this->content.=$this->doc->spacer(20).$this->doc->section('',$this->doc->makeShortcutIcon('id',implode(',',array_keys($this->MOD_MENU)),$this->MCONF['name']));
+			}
+
+			$this->content.=$this->doc->spacer(10);
+		} else {
+			// If no access or if ID == zero
+
+			$this->doc = t3lib_div::makeInstance('mediumDoc');
+			$this->doc->backPath = $BACK_PATH;
+
+			$this->content.=$this->doc->startPage($LANG->getLL('title'));
+			$this->content.=$this->doc->header($LANG->getLL('title'));
+			$this->content.=$this->doc->spacer(5);
+			$this->content.=$this->doc->spacer(10);
+		}
+	}
+
+	/**
+	 * Prints out the module HTML
+	 *
+	 * @return	void
+	 */
+	function printContent()	{
+
+		$this->content.=$this->doc->endPage();
+		echo $this->content;
+	}
+
+	/**
+	 * Generates the module content
+	 *
+	 * @return	void
+	 */
+	function moduleContent()	{
+		switch((string)$this->MOD_SETTINGS['function'])	{
+			case 1:
+				$this->moduleEventContent();
+			break;
+			case 2:
+				$this->moduleCommonContent();
+			break;
+			case 3:
+				$content='<div align=center><strong>Not implemented yet ...</strong></div>';
+				$this->content.=$this->doc->section('Session planning:',$content,0,1);
+			break;
+		}
+	}
+	
+	/**
+	 * Generates the content for event data
+	 *
+	 * @return	void
+	 */
+	function moduleEventContent()	{
+		global $BE_USER,$LANG;
+		
+		// define the sub modules that should be available in the tabmenu
+		$this->availableSubModules = array();
+
+		// only show the tabs if the back-end user has access to the corresponding tables
+		if ($BE_USER->check('tables_select', 'tx_wseevents_timeslots')) {
+			$this->availableSubModules[1] = $LANG->getLL('subModuleTitle_time_slots');
+		}
+		if ($BE_USER->check('tables_select', 'tx_wseevents_sessions')) {
+			$this->availableSubModules[2] = $LANG->getLL('subModuleTitle_sessions');
+		}
+		if ($BE_USER->check('tables_select', 'tx_wseevents_speaker_attendance')) {
+			$this->availableSubModules[3] = $LANG->getLL('subModuleTitle_speaker_attendance');
+		}
+		if ($BE_USER->check('tables_select', 'tx_wseevents_events')) {
+			$this->availableSubModules[4] = $LANG->getLL('subModuleTitle_events');
+		}
+
+		// Read the selected sub module (from the tab menu) and make it available within this class.
+		$this->subModule = intval(t3lib_div::_GET('subModule'));
+
+		// If $this->subModule is not a key of $this->availableSubModules,
+		// set it to the key of the first element in $this->availableSubModules
+		// so the first tab is activated.
+		if (!array_key_exists($this->subModule, $this->availableSubModules)) {
+			reset($this->availableSubModules);
+			$this->subModule = key($this->availableSubModules);
+		}
+
+		// Only generate the tab menu if the current back-end user has the
+		// rights to show any of the tabs.
+		if ($this->subModule) {
+			$this->content .= $this->doc->getTabMenu(array('id' => $this->id),
+				'subModule',
+				$this->subModule,
+				$this->availableSubModules);
+			$this->content .= $this->doc->spacer(5);
+		}
+
+		// Select which sub module to display.
+		// If no sub module is specified, an empty page will be displayed.
+		switch ($this->subModule) {
+			case 1:
+#					$eventsListClassname = t3lib_div::makeInstanceClassName('tx_wseevents_timeslots');
+#					$eventsList = new $eventsListClassname($this);
+#					$this->content .= $eventsList->show();
+				$this->content .= 'Edit time slots here<br />';
+				$this->content .= $LANG->getLL('not_implemented');
+				break;
+			case 2:
+				$this->content .= 'Edit sessions here<br />';
+				$this->content .= $LANG->getLL('not_implemented');
+				break;
+			case 3:
+				$this->content .= 'Edit speaker attendance here<br />';
+				$this->content .= $LANG->getLL('not_implemented');
+				break;
+			case 4:
+				$this->content .= 'Edit events here<br />';
+				$this->content .= $LANG->getLL('not_implemented');
+				break;
+			default:
+				$this->content .= '';
+				break;
+		}
+	}
+
+	/**
+	 * Generates the content for common data
+	 *
+	 * @return	void
+	 */
+	function moduleCommonContent()	{
+		global $BE_USER,$LANG;
+		
+		// define the sub modules that should be available in the tabmenu
+		$this->availableSubModules = array();
+
+		// only show the tabs if the back-end user has access to the corresponding tables
+		if ($BE_USER->check('tables_select', 'tx_wseevents_locations')) {
+			$this->availableSubModules[1] = $LANG->getLL('subModuleTitle_locations');
+		}
+		if ($BE_USER->check('tables_select', 'tx_wseevents_rooms')) {
+			$this->availableSubModules[2] = $LANG->getLL('subModuleTitle_rooms');
+		}
+		if ($BE_USER->check('tables_select', 'tx_wseevents_speakers')) {
+			$this->availableSubModules[3] = $LANG->getLL('subModuleTitle_speakers');
+		}
+		if ($BE_USER->check('tables_select', 'tx_wseevents_categories')) {
+			$this->availableSubModules[4] = $LANG->getLL('subModuleTitle_categories');
+		}
+
+		// Read the selected sub module (from the tab menu) and make it available within this class.
+		$this->subModule = intval(t3lib_div::_GET('subModule'));
+
+		// If $this->subModule is not a key of $this->availableSubModules,
+		// set it to the key of the first element in $this->availableSubModules
+		// so the first tab is activated.
+		if (!array_key_exists($this->subModule, $this->availableSubModules)) {
+			reset($this->availableSubModules);
+			$this->subModule = key($this->availableSubModules);
+		}
+
+		// Only generate the tab menu if the current back-end user has the
+		// rights to show any of the tabs.
+		if ($this->subModule) {
+			$this->content .= $this->doc->getTabMenu(array('id' => $this->id),
+				'subModule',
+				$this->subModule,
+				$this->availableSubModules);
+			$this->content .= $this->doc->spacer(5);
+		}
+
+		// Select which sub module to display.
+		// If no sub module is specified, an empty page will be displayed.
+		switch ($this->subModule) {
+			case 1:
+#					$eventsListClassname = t3lib_div::makeInstanceClassName('tx_wseevents_timeslots');
+#					$eventsList = new $eventsListClassname($this);
+#					$this->content .= $eventsList->show();
+				$this->content .= 'Edit locations here<br />';
+				$this->content .= $LANG->getLL('not_implemented');
+				break;
+			case 2:
+				$this->content .= 'Edit rooms here<br />';
+				$this->content .= $LANG->getLL('not_implemented');
+				break;
+			case 3:
+				$this->content .= 'Edit speaker here<br />';
+				$this->content .= $LANG->getLL('not_implemented');
+				break;
+			case 4:
+				$this->content .= 'Edit categories here<br />';
+				$this->content .= $LANG->getLL('not_implemented');
+				break;
+			default:
+				$this->content .= '';
+				break;
+		}
+	}
+	
+}
 
 
 
