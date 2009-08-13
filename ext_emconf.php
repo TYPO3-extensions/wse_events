9a47afe082053271261c6aadbb61e0892f1a3f91
amp4ecs <amp4ecs@1bbec0d5-414c-dc11-ae90-00e081727c95>
2009-08-13 15:37:00 +0000

    Version 0.2.0 Upload to warpstock.eu
    git-svn-id: http://svn.netlabs.org/repos/wse_events/trunk@161 1bbec0d5-414c-dc11-ae90-00e081727c95

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "wse_events"
 #
-# Auto generated 23-11-2008 20:26
+# Auto generated 13-08-2009 17:07
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'module' => 'mod1',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => 1,
 	'createDirs' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.17',
+	'version' => '0.2.0',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:53:{s:9:"ChangeLog";s:4:"31cf";s:10:"README.txt";s:4:"d74e";s:33:"class.tx_wseevents_categories.php";s:4:"b23e";s:31:"class.tx_wseevents_dbplugin.php";s:4:"fb60";s:29:"class.tx_wseevents_events.php";s:4:"38c6";s:32:"class.tx_wseevents_locations.php";s:4:"07c4";s:28:"class.tx_wseevents_rooms.php";s:4:"ca3d";s:31:"class.tx_wseevents_speakers.php";s:4:"f294";s:36:"class.tx_wseevents_tcemainprocdm.php";s:4:"4acd";s:32:"class.tx_wseevents_timeslots.php";s:4:"e67d";s:12:"ext_icon.gif";s:4:"138e";s:17:"ext_localconf.php";s:4:"bc90";s:15:"ext_php_api.dat";s:4:"e177";s:14:"ext_tables.php";s:4:"b4fa";s:14:"ext_tables.sql";s:4:"1ff1";s:19:"flexform_ds_pi1.xml";s:4:"1335";s:32:"icon_tx_wseevents_categories.gif";s:4:"138e";s:28:"icon_tx_wseevents_events.gif";s:4:"138e";s:31:"icon_tx_wseevents_locations.gif";s:4:"138e";s:27:"icon_tx_wseevents_rooms.gif";s:4:"138e";s:30:"icon_tx_wseevents_sessions.gif";s:4:"138e";s:30:"icon_tx_wseevents_speakers.gif";s:4:"138e";s:31:"icon_tx_wseevents_timeslots.gif";s:4:"138e";s:16:"locallang_db.xml";s:4:"37b0";s:7:"tca.php";s:4:"1db5";s:8:"temp.txt";s:4:"9a43";s:8:"todo.txt";s:4:"5641";s:36:"tx_wseevents_speakerrestrictions.gif";s:4:"138e";s:14:"warpstock.tmpl";s:4:"9bc3";s:14:"wseevents.tmpl";s:4:"cb17";s:14:"doc/manual.sxw";s:4:"03ca";s:19:"doc/wizard_form.dat";s:4:"9500";s:20:"doc/wizard_form.html";s:4:"25ab";s:39:"mod1/class.tx_wseevents_backendlist.php";s:4:"5d52";s:42:"mod1/class.tx_wseevents_categorieslist.php";s:4:"acbb";s:38:"mod1/class.tx_wseevents_eventslist.php";s:4:"20f0";s:41:"mod1/class.tx_wseevents_locationslist.php";s:4:"ca78";s:37:"mod1/class.tx_wseevents_roomslist.php";s:4:"9666";s:43:"mod1/class.tx_wseevents_sessionplanning.php";s:4:"9930";s:40:"mod1/class.tx_wseevents_sessionslist.php";s:4:"d07c";s:51:"mod1/class.tx_wseevents_speakerrestrictionslist.php";s:4:"6179";s:40:"mod1/class.tx_wseevents_speakerslist.php";s:4:"ed2b";s:41:"mod1/class.tx_wseevents_timeslotslist.php";s:4:"59aa";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"a216";s:14:"mod1/index.php";s:4:"ce3e";s:18:"mod1/locallang.xml";s:4:"9097";s:22:"mod1/locallang_mod.xml";s:4:"1826";s:19:"mod1/moduleicon.gif";s:4:"dcb4";s:30:"pi1/class.tx_wseevents_pi1.php";s:4:"a935";s:17:"pi1/locallang.xml";s:4:"6f7f";s:24:"pi1/static/editorcfg.txt";s:4:"87c1";s:20:"pi1/static/setup.txt";s:4:"06fa";}',
+	'_md5_values_when_last_written' => 'a:53:{s:9:"ChangeLog";s:4:"e000";s:10:"README.txt";s:4:"d74e";s:33:"class.tx_wseevents_categories.php";s:4:"cc4b";s:31:"class.tx_wseevents_dbplugin.php";s:4:"cc15";s:29:"class.tx_wseevents_events.php";s:4:"2330";s:32:"class.tx_wseevents_locations.php";s:4:"53ce";s:28:"class.tx_wseevents_rooms.php";s:4:"ca3d";s:31:"class.tx_wseevents_speakers.php";s:4:"aa6a";s:36:"class.tx_wseevents_tcemainprocdm.php";s:4:"7fd5";s:32:"class.tx_wseevents_timeslots.php";s:4:"78c6";s:12:"ext_icon.gif";s:4:"138e";s:17:"ext_localconf.php";s:4:"f471";s:15:"ext_php_api.dat";s:4:"4a27";s:14:"ext_tables.php";s:4:"97b4";s:14:"ext_tables.sql";s:4:"1ff1";s:19:"flexform_ds_pi1.xml";s:4:"1335";s:32:"icon_tx_wseevents_categories.gif";s:4:"138e";s:28:"icon_tx_wseevents_events.gif";s:4:"138e";s:31:"icon_tx_wseevents_locations.gif";s:4:"138e";s:27:"icon_tx_wseevents_rooms.gif";s:4:"138e";s:30:"icon_tx_wseevents_sessions.gif";s:4:"138e";s:30:"icon_tx_wseevents_speakers.gif";s:4:"138e";s:31:"icon_tx_wseevents_timeslots.gif";s:4:"138e";s:16:"locallang_db.xml";s:4:"37b0";s:7:"tca.php";s:4:"21a1";s:8:"temp.txt";s:4:"9a43";s:8:"todo.txt";s:4:"74c1";s:36:"tx_wseevents_speakerrestrictions.gif";s:4:"138e";s:14:"warpstock.tmpl";s:4:"9bc3";s:14:"wseevents.tmpl";s:4:"cb17";s:14:"doc/manual.sxw";s:4:"03ca";s:19:"doc/wizard_form.dat";s:4:"9500";s:20:"doc/wizard_form.html";s:4:"25ab";s:39:"mod1/class.tx_wseevents_backendlist.php";s:4:"6635";s:42:"mod1/class.tx_wseevents_categorieslist.php";s:4:"32af";s:38:"mod1/class.tx_wseevents_eventslist.php";s:4:"4aaf";s:41:"mod1/class.tx_wseevents_locationslist.php";s:4:"6646";s:37:"mod1/class.tx_wseevents_roomslist.php";s:4:"e595";s:43:"mod1/class.tx_wseevents_sessionplanning.php";s:4:"3984";s:40:"mod1/class.tx_wseevents_sessionslist.php";s:4:"e647";s:51:"mod1/class.tx_wseevents_speakerrestrictionslist.php";s:4:"7983";s:40:"mod1/class.tx_wseevents_speakerslist.php";s:4:"def1";s:41:"mod1/class.tx_wseevents_timeslotslist.php";s:4:"67fb";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"56fa";s:14:"mod1/index.php";s:4:"3962";s:18:"mod1/locallang.xml";s:4:"9097";s:22:"mod1/locallang_mod.xml";s:4:"1826";s:19:"mod1/moduleicon.gif";s:4:"dcb4";s:30:"pi1/class.tx_wseevents_pi1.php";s:4:"c5ce";s:17:"pi1/locallang.xml";s:4:"6f7f";s:24:"pi1/static/editorcfg.txt";s:4:"87c1";s:20:"pi1/static/setup.txt";s:4:"06fa";}',
 	'suggests' => array(
 	),
 );
